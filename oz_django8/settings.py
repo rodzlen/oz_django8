06f57d71362fb9fed739c0320d33043946473245
rodzlen <qkqhrkxdkdy@naver.com>
2025-02-26 13:54:54 +0900

    Install admin

--- oz_django8/settings.py
+++ oz_django8/settings.py
@@ -35,6 +35,7 @@ ALLOWED_HOSTS : list[str] = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
+    "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
