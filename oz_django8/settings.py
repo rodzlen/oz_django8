243789a1e5459b6383c5a81ec7e9b1658dfeafe0
rodzlen <qkqhrkxdkdy@naver.com>
2025-02-26 13:38:38 +0900

    Introduce mypy

--- oz_django8/settings.py
+++ oz_django8/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
+from typing import reveal_type
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -27,7 +29,7 @@ SECRET_KEY = (
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS : list[str] = ["*"]
 
 
 # Application definition
