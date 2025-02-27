56b025081bbc9793b5944f44476c912cef2c46ba
rodzlen <qkqhrkxdkdy@naver.com>
2025-02-27 13:36:27 +0900

    implement do_like

---
+++ tabom/tests/test_like_service.py
@@ -0,0 +1,20 @@
+from django.test import TestCase
+
+from tabom.models import Article, User
+from tabom.services.like_service import do_like
+
+
+class TestLikeService(TestCase):
+    def test_a_user_con_like_an_article(self) -> None:
+        # Given: 테스트에 필요한 재료를 준비
+        user = User.objects.create(name="test")
+        article = Article.objects.create(title="test_title")
+        # When : 실제 테스트 대상이 되는 동작을 실행
+        like = do_like(user.id, article.id)
+
+        # Then : 동작을 마친 후 결과가 에상한 대로 나왔는지 검증
+        # if guard
+        assert user.id == like.user_id
+        self.assertIsNotNone(like.id)
+        self.assertEqual(user.id, like.user_id)
+        self.assertEqual(article.id, like.article_id)
